97a7ce7209e7dfbcddfd8cd4e491be05f21caa16
xcao2005 <xcao2005@gmail.com>
2023-11-28 23:16:19 -0500

    setup Button.h and Button.cpp; sprite tentative as need to implement sprite later

--- Button.cpp
+++ Button.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "Button.h"
+
+#include <utility>
+
+
+//tentative: just for setup
+Button::Button(sf::Vector2f _position, std::function<void(void)> _onClick): position(_position), sprite(nullptr), onClickCallback(_onClick){}
+
+sf::Vector2f Button::getPosition() {
+    return position;
+}
+
+sf::Sprite* Button::getSprite() {
+    return sprite;
+}
+
+void Button::setSprite(sf::Sprite* _sprite){
+    sprite=_sprite;
+}
+
+void Button::onClick(){
+    onClickCallback();
+}
