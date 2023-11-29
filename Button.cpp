fa8560cc241a2426c81c2981e63efb02e05557df
xcao2005 <xcao2005@gmail.com>
2023-11-28 21:28:30 -0500

    initialize Button class, set up Button.h

---
+++ Button.cpp
@@ -0,0 +1,5 @@
+//
+// Created by xcao2 on 11/28/2023.
+//
+
+#include "Button.h"
---
+++ Button.h
@@ -0,0 +1,26 @@
+//
+// Created by xcao2 on 11/28/2023.
+//
+
+#ifndef COP_PROJECT4_BUTTON_H
+#define COP_PROJECT4_BUTTON_H
+
+#include <SFML/Graphics.hpp>
+#include <functional>
+
+
+class Button {
+public:
+    Button(sf::Vector2f _position, std::function<void(void)> _onClick);
+    sf::Vector2f getPosition();
+    sf::Sprite* getSprite();
+    void setSprite(sf::Sprite* _sprite);
+    void onClick();
+private:
+    sf::Vector2f position;
+    sf::Sprite* sprite;
+    std::function<void(void)> onClickCallback;
+};
+
+
+#endif //COP_PROJECT4_BUTTON_H
