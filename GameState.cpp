d3f5dae91ff2272707f04c63398568f2476d456c
xcao2005 <xcao2005@gmail.com>
2023-11-29 20:30:34 -0500

    set up GameState.cpp, write GameState::getTile, calculating index

--- GameState.cpp
+++ GameState.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "GameState.h"
+#include <SFML/Graphics.hpp>
+
+GameState::GameState(sf::Vector2i _dimensions, int _numberOfMines) : dimensions(_dimensions), numberOfMines(_numberOfMines), flagCount(0), playStatus(PLAYING) {
+    // random board
+}
+
+GameState::GameState(const char* filepath) : playStatus(PLAYING) {
+    // given board
+}
+
+int GameState::getFlagCount() {
+    return flagCount;
+}
+
+int GameState::getMineCount() {
+    return numberOfMines;
+}
+
+Tile* GameState::getTile(int x, int y) {
+    // if exists
+    if (x >= 0 && x < dimensions.x && y >= 0 && y < dimensions.y) {
+        // calculate index 2D->1D
+        int index = x + y * dimensions.x;
+        return &tiles[index];
+    }
+    return nullptr;
+}
+
+GameState::PlayStatus GameState::getPlayStatus() {
+    return playStatus;
+}
+
+void GameState::setPlayStatus(PlayStatus _status) {
+    playStatus = _status;
+}
