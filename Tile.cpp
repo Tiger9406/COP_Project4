e865a8f5889cbae5a856fe7dc6a5d43a85066656
xcao2005 <xcao2005@gmail.com>
2023-11-29 20:14:51 -0500

    set up Tile.cpp

--- Tile.cpp
+++ Tile.cpp
@@ -3,3 +3,55 @@
 //
 
 #include "Tile.h"
+
+Tile::Tile(sf::Vector2f _position) : position(_position), currentState(HIDDEN) {
+    //maybe more code later
+}
+
+sf::Vector2f Tile::getLocation() {
+    return position;
+}
+
+Tile::State Tile::getState() {
+    return currentState;
+}
+
+std::array<Tile*, 8>& Tile::getNeighbors() {
+    return neighbors;
+}
+
+void Tile::setState(State _state) {
+    currentState = _state;
+    // maybe more actions later
+}
+
+void Tile::setNeighbors(std::array<Tile*, 8> _neighbors) {
+    neighbors = _neighbors;
+}
+
+void Tile::onClickLeft() {
+    if (currentState == HIDDEN) {
+        // action depending on what it is
+    }
+}
+
+void Tile::onClickRight() {
+    if (currentState == HIDDEN) {
+        currentState = FLAGGED;
+    } else if (currentState == FLAGGED) {
+        currentState = HIDDEN;
+    }
+    // lower flag count
+}
+
+void Tile::draw() {
+    // probably used in a lot of places: draw with sfml
+}
+
+void Tile::revealNeighbors() {
+    for (Tile* neighbor : neighbors) {
+        if (neighbor && neighbor->getState() == HIDDEN) {
+            // set neighbor status/actions
+        }
+    }
+}
